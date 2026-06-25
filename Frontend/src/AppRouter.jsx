8693b5becba5dd44d968ea06ce517f001d32fef8
Dardeka <ddkwari2@gmail.com>
2026-06-25 21:10:02 +0700

    Feat: update title in index.html and refactor App component to use AppRouter

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Materi from "./pages/HalamanMateri";
+import Kuis from "./pages/Kuis";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function AppRouter() {
-    <>
+    return(
         <Router>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="/materi" element={<Materi />} />
+                <Route path="/materi" element={<Materi/>} />
                 <Route path="/kuis" element={<Kuis />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
-    </>
+    )
+    
 }
+
+export default AppRouter;
